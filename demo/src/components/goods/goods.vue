1a25d584fe653a1c3fb5dc345360ab7e3fde0b80
王霞 <827472489@qq.com>
2020-08-18 18:42:05 +0800

    周二

--- demo/src/components/goods/goods.vue
+++ demo/src/components/goods/goods.vue
@@ -1,14 +1,121 @@
 <template>
   <div class="goods">
+
     <div class="menu-wrapper">
       <ul>
-        <li v-for="item in goods" :key="item.name" class="caidan">
-          {{item.name}}
-        </li>
+         <li v-for="item in goods" :key="item.name" class="caidan">
+          <span class="text">{{item.name}}</span>
+        </li> 
       </ul>
-      <div class="foods-wrapper"></div>
+      </div>
+<!-- 1 -->
+      <div class="foods-wrapper">
+        <div class="foods-title">
+          <span>单人特色套餐</span>
+        </div>
+
+        <div class="foods-item">
+          <div class="left">
+             <img src="../../../src/assets/01.png"/>
+          </div>
+          <div class="right">
+            <div class="foods-name">
+              <span>皮蛋瘦肉粥配包子套餐</span>
+            </div>
+
+             <div class="foods-description">
+              <span>咸粥</span>
+            </div>
+
+            <div class="foods-xiaoliang">
+              <span>月售1132份</span>
+              <span style="margin-left:12px;">好评率100%</span>
+            </div>
+
+            <div class="foods-jiage">
+              <span>￥24</span>
+              <span style="text-decoration:line-through;color:rgb(147,153,159);line-height:24px;font-size:10px;font-weight:normal;" >￥28</span>
+            </div>
+          </div>
+        </div>
+<!-- 2 -->
+       <div class="foods-title">
+          <p>特色粥品</p>
+        </div>
+
+        <div class="foods-item">
+          <div class="left">
+            <img src="../../../src/assets/02.png"/>
+          </div>
+          <div class="right">
+            <div class="foods-name">
+              <span>莲子核桃黑米粥</span>
+            </div>
+
+            <div class="foods-xiaoliang">
+              <span>月售1132份</span>
+              <span style="margin-left:12px;">好评率100%</span>
+            </div>
+
+            <div class="foods-jiage">
+              <span>￥10</span>
+            </div>
+          </div>
+        </div>
+ <!-- 3 -->
+       <div class="foods-item">
+          <div class="left">
+             <img src="../../../src/assets/03.png"/>
+          </div>
+          <div class="right">
+            <div class="foods-name">
+              <span>雪梨银耳百合粥</span>
+            </div>
+            <div class="foods-xiaoliang">
+              <span class="xiaoliang">月售1132份</span>
+              <span style="margin-left:12px;">好评率100%</span>
+            </div>
+
+            <div class="foods-jiage">
+              <span>￥24</span>
+            </div>
+          </div>
+        </div>
+       <div class="foods-item">
+          <div class="left">
+             <img src="../../../src/assets/04.png"/>
+          </div>
+          <div class="right">
+            <div class="foods-name">
+              <span>南瓜粥</span>
+            </div>
+            <div class="foods-xiaoliang">
+              <span>月售1132份</span>
+              <span style="margin-left:12px;">好评率100%</span>
+            </div>
+
+            <div class="foods-jiage">
+              <span>￥20</span>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="shop">
+        <div class="shopleft">
+          <div class="shoplogo">
+            <img src="../../../src/assets/05.png"/>
+          </div>
+          <div class="lala">￥0</div>
+        </div>
+        <div class="shopcenter">
+          <span>另需配送费￥4元</span>
+        </div>
+         <div class="shopright">
+            <span>￥20元起送</span>
+         </div>
+      </div>
     </div>
-  </div>
+    
 </template>
 
 <script>
@@ -28,18 +135,150 @@ export default {
   position: absolute;
   width: 100%;
   top: 174px;
-  bottom: 46px;
+  bottom: 60px;
   /* 超出部分隐藏 */
   overflow: hidden;
+  left:0px;
 }
 .menu-wrapper {
   /* 第一个参数是等分，第二个参数是根据内容的缩放情况，第三个参数是占位大小 */
   flex: 0 0 80px;
   width: 80px;
   background: #f3f5f7;
+  text-align: center;
+  
+}
+.caidan{
+  width: 100%;
+  height: 54px;
+  border-bottom: 1px solid gainsboro;
+  line-height: 14px;
+  display: table;
+ 
+}
+.text{
+  font-size:12px ;
+  font-weight: 200;
+  line-height: 14px;
+  color: rgb(77, 85, 93);
+  display: table-cell;
+  vertical-align: middle;
 }
 .foods-wrapper {
   flex: 1;
 }
+.foods-title{
+  width: 100%;;
+  height: 30px;
+  background: #f3f5f7;
+  color: rgb(77, 85, 93);
+  border-left: 3px solid gray;
+  padding-left: 20px;
+   line-height: 2; 
+}
+.foods-item{
+  display: flex;
+  width: 100%;
+  height: 100px;
+  border-bottom:1px solid  #f3f5f7 ;
+  /* margin: 18px 0 18px 18px;  */
 
+}
+.left{
+  width: 60px;
+  height: 60px;
+  margin-right: 10px;
+}
+.left img{
+  width: 100%;
+  margin-top: 18px;
+  margin-left: 18px;
+}
+.foods-jiage span{
+  color: red;
+  font-weight: normal;
+  font-size: 14px;
+  line-height: 24px;
+}
+.foods-name span{
+  font-size: 14px;
+  color: rgb(16, 27, 37);
+  line-height: 14px;
+}
+.foods-xiaoliang span{
+  font-size: 10px;
+  color: rgb(147, 153, 159);
+  line-height: 10px;
+}
+.foods-description span{
+  font-size: 10px;
+  color: rgb(147, 153, 159);
+  line-height: 10px;
+}
+.right{
+  height: 60px;
+  margin-top: 18px;
+  margin-left: 20px;
+}
+.shop{
+  position: fixed;
+  left: 0;
+  bottom: 0;
+  z-index:50;
+  width: 100%;
+  height: 48px;
+  display: flex;
+}
+.shopleft{
+  flex:1;
+  background-color: #141d27 ;
+  
+}
+.shoplogo{
+background-color:rgb(255, 255, 255,0.4);
+  position: relative;
+  top: -10px;
+  margin: 0 12px;
+  padding: 6px;
+  width: 56px;
+  height: 56px;
+  box-sizing: border-box;
+  vertical-align: top;
+  border-radius: 50%;
+}
+.shoplogo img{
+  margin-top: 6px;
+  margin-left: 5px;
+}
+.lala{
+  display: inline-block;
+  font-size: 16px;
+  color: rgb(255, 255, 255,0.4);
+  font-weight: 700;
+  line-height: 24px;
+}
+.shopcenter{
+  /* flex: 1; */
+   background-color: #141d27;
+   border-left:1px solid rgb(255, 255, 255,0.1) ;
+   width: 150px;
+}
+.shopcenter span{
+  font-size: 16px;
+  color: rgb(255, 255, 255,0.4);
+  font-weight: 700;
+  line-height: 3;
+  text-align: center;
+}
+.shopright{
+   background-color:rgb(77, 85, 93);
+   text-align: center;
+   width: 105px;
+}
+.shopright span{
+  font-size: 12px;
+  color: rgb(255, 255, 255,0.4);
+  font-weight: 700;
+  line-height: 4;
+}
 </style>
--- demo/src/components/header/header.vue
+++ demo/src/components/header/header.vue
@@ -18,21 +18,85 @@
             <span class="text">{{seller.supports[0].description}}</span>
         </div>
 
-    <div v-if="seller.supports" class="support-count">
+    <div v-if="seller.supports" class="support-count" @click="showDetail">
         <span class="count">{{seller.supports.length}}个</span>
         <i class="tubiao"> > </i>
-
     </div>
     
     
     </div>
 </div>
-<div class="bulletin-wrapper">
+<div class="bulletin-wrapper" @click="showDetail">
     <div class="bulletin-title"></div>
     <div class="bulletin-text">{{seller.bulletin}}</div>
     <i class="bulletin-jiantou">></i>
 </div>
+<div v-show="detailShow" class="detail">
+    <div class="detail-wrapper">
+        <div class="detail-main">
+            <div class="main-name"> 
+                粥品香坊（大运村）
+            </div>
+            <div class="main-star">
+                <div class="star">
+                    <img src="../../../src/assets/star24_on@2x.png"/>
+                </div>
+                <div class="star">
+                    <img src="../../../src/assets/star24_on@2x.png"/>
+                </div>
+                <div class="star">
+                    <img src="../../../src/assets/star24_on@2x.png"/>
+                </div>
+                <div class="star">
+                    <img src="../../../src/assets/star24_on@2x.png"/>
+                </div>
+                <div class="star">
+                    <img src="../../../src/assets/star24_off@2x.png"/>
+                </div>
+
+            </div>
+        <div class="main-title">
+            <div class="title-left"></div>
+            <div class="title-center">优惠信息</div>
+            <div class="title-right"></div>
+        </div>
+        <div class="main-menu">
+            <div class="menu-item">
+               <img src="../../../src/assets/decrease_1@2x.png"/>
+                在线支付满28减5，满50减10</div>
+            <div class="menu-item">
+                 <img src="../../../src/assets/discount_1@2x.png"/>
+                单人精彩赛</div>
+            <div class="menu-item">
+                 <img src="../../../src/assets/discount_1@2x.png"/>
+                清肺雪梨汤8折抢购</div>
+            <div class="menu-item">
+                 <img src="../../../src/assets/special_1@2x.png"/>
+                特价饮品八折抢购</div>
+            <div class="menu-item">
+                 <img src="../../../src/assets/special_1@2x.png"/>
+                单人特色套餐</div>
+        </div>
+
+          <div class="main-title">
+            <div class="title-left"></div>
+            <div class="title-center">商家公告</div>
+            <div class="title-right"></div>
+        </div>
+        <div class="main-menu2">
+            <div class="menu-item2">粥品香坊其烹饪粥料的秘方源于中国千年古法，在融和现代制作工艺，由世界烹饪大师屈浩先生领衔研发。
+                坚守纯天然、0添加的良心品质深得消费者青睐，发展至今成为粥类的引领品牌。是2008年奥运会和2013年园博会指定餐饮服务商。</div>
+        </div>
+        </div>
+    </div>
+
+
+    <div class="detail-close" @click="hideDetail">
+            <i class="icon-close"><img src="../../../src/assets/06.png"/> </i> 
+    </div>
 </div>
+</div>
+
 </template>
 
 <script>
@@ -43,7 +107,22 @@ props: {
         type:Object
     }
 
-}
+},
+data() {
+    return {
+        detailShow: false
+    };
+},
+methods: {
+    // 当点击时，详情页显示
+    showDetail(){
+        this.detailShow = true;
+    },
+    hideDetail() {
+        this.detailShow = false;
+    }
+ 
+ }
 };
 </script>
 
@@ -94,7 +173,7 @@ border-radius: 2px;
 }
 .text{
  font-size: 10px;
-   color: rgba(255, 255, 255);
+   color: rgba(255, 255, 255);;
    line-height: 12px;
    font-weight: 200;
 }
@@ -133,5 +212,107 @@ font-size: 10px;
 }  */
 .bulletin-text{
     font-size: 10px;
+}
+.detail{
+    position: fixed;
+    z-index:100;
+    width: 100%;
+    height: 100%;
+    overflow: auto;
+    background-color: rgba(7, 17, 27, 0.8);
+    top: 0;
+    left: 0;
+}
+ .detail-wrapper{
+    
+    min-height: 100%;
+}
+.detail-main{
+    margin-top: 64px;
+    padding-bottom: 64px;
+} 
+.detail-close{
+    position: relative;
+    width: 32px;
+    height: 32px;
+    margin: -64px auto 0 auto;
+    clear: both;
+    font-size: 32px;
+
+} 
+.main-name{
+    font-size: 16px;
+    font-weight: 700;
+    color: rgb(255,255,255);
+    line-height: 16px;
+    margin-top: 64px;
+    text-align: center;
+}
+.main-star{
+    width: 55%;
+    height: 24px;
+    margin-top: 16px;  
+    margin-left:25% ;
+    display: flex;
+}
+.star{
+    flex: 1;
+}
+.main-title{
+    display: flex;
+    margin-top: 15px;
+    width: 100%;
+    margin-bottom: 32px;
+     
+}
+.title-left{
+    width: 150px;
+    height: 2px;
+    background-color:rgba(255, 255, 255,0.2);
+    margin-left: 20px;
+    margin-top: 12px;
+    
+}
+.title-center{
+    width: 90px;
+    height: 22px;
+    margin-left: 12px;
+    margin-right: 12px;
+    
+}
+.title-right{
+    width: 150px;
+    height: 2px;
+    background-color: rgba(255, 255, 255,0.2);
+    margin-top: 12px;
+    margin-right: 20px;
+}
+.main-menu{
+    width: 100%;
+    height: 180px;
+}
+.menu-item{
+    height: 25px;
+    width: 100%;
+    margin-left: 40px;
+    margin-bottom: 12px;
+    font-size: 12px;
+    font-weight: 200;
+    color: rgb(255,255,255);
+    line-height: 12px;
+}
+.main-menu2{
+    width: 100%;
+    height: 180px;
+}
+.menu-item2{
+    height: 180px;
+    width: 80%;
+    margin-left: 40px;
+    margin-bottom: 12px;
+    font-size: 12px;
+    font-weight: 200;
+    color: rgb(255,255,255);
+    line-height: 24px;
 }
 </style>
